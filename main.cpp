a61cba0e73757f812bc478ca8c0b3da4610c7315
Juan Aparicio <apariciojuan30@gmail.com>
2016-02-09 22:42:46 -0300

    cambios para poner color en la consola

--- main.cpp
+++ main.cpp
@@ -1,9 +1,54 @@
 //Inclusion de librerias
 #include <stdio.h>
-#include "String.h"
+//#include "String.h"
+#include<windows.h>
+#include "Ecuacion.h"
+//TODO hacer modulo del arbol de ecuaciones
+
+
+//Funciones para primer entrega
+void ayuda(){ //TODO preguntar si tengo que hacer sobrecarga o hago un switch case aca adentro segun lo que ingrese
+    /*Para poner el texto en un color determinado bajo windows
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    WORD saved_attributes;
+
+    // Save current attributes
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    saved_attributes = consoleInfo.wAttributes;
+
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+    printf("This is some nice COLORFUL text, isn't it?");
+
+    // Restore original attributes
+    SetConsoleTextAttribute(hConsole, saved_attributes);
+    printf("Back to normal");
+
+    printf("\x1b[31m Hello World \x1b[0m\n");
+    printf("\033[22;34mHello, world!\033[0m");*/
+
+    printf("----------\n|Comandos|\n----------\n");
+
+    printf("\ncrear nombreFuncion ax + b= 0  --> Crea la funcion ingresada con el nombre nombreFuncion");
+
+    printf("\nlistar nombreFuncion  --> ");//TODO preguntar a bea, incosistencia en la explicacion de ejemplos y el ejemplo de consola
+
+    printf("\nreducir nombreFuncion nuevoNombreReducida  --> Reduce la ecuacion nombreFuncion y la guarda en nuevoNombreReducida");
+
+    printf("\nresolver nombreFuncion  --> Resuelve la ecuacion nombreFuncion");
+
+    printf("\nsumar nombreFuncionA nombreFuncionB nuevoNombreSumado  -->Suma las funciones nombreFuncionA y nombreFuncionB, y guarda el resultado en NuevoNombreSumado");
+
+    printf("\nguardar nombreFuncion nombreArchivo.dat  -->Guarda la ecuacion NombreFuncion en el archivo nombreArchivo.dat");
+
+    printf(""); //TODO preguntar a bea que onda con el comando recuperar
+
+    printf("\n salir  -->Finaliza la ejecucion del programa");
+}
 
 
 int main(){
+    ayuda();
     String cargado;
     String_crear(cargado);
 
@@ -11,11 +56,6 @@ int main(){
     while(true){
         printf("\nIngrese un String: ");
         String_cargar(cargado);
-//        printf("\n");
-//        String_mostrar(cargado);
-//        String_addChar(cargado, '1');
-//        printf("\n");
-//        String_mostrar(cargado);
         if(String_isNumeric(cargado))
             printf("true");
         else
